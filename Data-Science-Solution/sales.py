edd8627ada579190928b7bff8e4ecdffd9d3ba05
anapauor <anapaula.orri@gmail.com>
2021-01-10 22:55:06 +0100

    /usr/bin/python3 /home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/sales.py
    /usr/bin/python3 /home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/sales.py
    /usr/bin/python3 /home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/sales.py
    cccccccc

--- Data-Science-Solution/sales.py
+++ Data-Science-Solution/sales.py
@@ -3,33 +3,43 @@ import pandas as pd
 import os
 
 # Sale resport class for report generation from files 
+
 class SalesReport():
     """Genererates reports of mothly sales based on cvs data files"""
 
     def __init__(self, files, path):
         self.files = files # name of the input files
         self.path = path # name of the input path
+        self.all_months_data = self.get_joined_monthly_data()
         
-    def join_monthly_data(self, new_file_name, op_path): 
+    def get_joined_monthly_data(self): 
         "Joints the monthly sales data in a single file named in 'new_file_name' located in the 'op_path'"
-        all_months_data = pd.DataFrame()
+        self.all_months_data = pd.DataFrame()
         for file in self.files:
             current_data = pd.read_csv(self.path+"/"+file)
-            all_months_data = pd.concat([all_months_data, current_data])
-        all_months_data.to_csv((os.path.join(op_path, new_file_name+ '.csv')), index=False)
-        return all_months_data
-        
-    def clean_empty_val(self, data): 
+            self.all_months_data = pd.concat([self.all_months_data, current_data])
+        return self.all_months_data
+
+    def save_joined_monthly_data(self, new_file_name, op_path):
+        self.all_months_data.to_csv((os.path.join(op_path, new_file_name+ '.csv')), index=False)
+
+    def clean_empty_val(self): 
         'Pre-processing data'
-        data = data.dropna(how='all') # Find and remove NAN
-        data['Month'] = pd.to_datetime(data['Order Date']).dt.month # Add a month column
-        def get_city(address): # Func to extract the city adress
-            return address.split(",")[1].strip(" ")
-        def get_state(address): # Func to extract the state adress
-            return address.split(",")[2].split(" ")[1]
-        # Generate colum for city (state)
-        data['City'] = data['Purchase Address'].apply(lambda x: f"{get_city(x)}  ({get_state(x)})")
-        return data
+        self.all_months_data = self.all_months_data.dropna(how='all') # Find and remove NAN
+        return self.all_months_data
+
+    # def set_month_column(self):
+    #     self.all_months_data['Month'] = pd.to_datetime(self.all_months_data['Order Date']).dt.month # Add a month column
+
+
+    # def get_address(self):
+    #     def get_city(address): # Func to extract the city adress
+    #         return address.split(",")[1].strip(" ")
+    #     def get_state(address): # Func to extract the state adress
+    #         return address.split(",")[2].split(" ")[1]
+    #     #  Create colum for city (state) applying get_city and get_state
+    #     self.all_months_data['City'] = self.all_months_data['Purchase Address'].apply(lambda x: f"{get_city(x)}  ({get_state(x)})")
+    #     return self.all_months_data
 
 
 
--- Data-Science-Solution/test.py
+++ Data-Science-Solution/test.py
@@ -9,10 +9,10 @@ import os
 
 path_test = "/home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/SalesAnalysis/Sales_Data"
 files_test = [file for file in os.listdir(path_test) if not file.startswith('.')] # Ignore hidden files
+
 Report1 = SalesReport(files=files_test, path=path_test)
 new_name_file = 'all_data_sales'
 op_path = '/home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/Output'
-df = (pd.read_csv(os.path.join(op_path, new_name_file + '.csv'))).reset_index(drop=True)
 
 
 # -------------------------------> TEST <---------------------------------------------------------------
@@ -25,13 +25,16 @@ class TestSalesReport(unittest.TestCase):
     def test_SalesReport_path(self):
         self.assertEqual(Report1.path, path_test)
     
-    def test_join_monthly_data(self):
-        jointed = (Report1.join_monthly_data(new_name_file, op_path)).reset_index(drop=True)
-        assert_frame_equal(jointed.reset_index(drop=True), df.reset_index(drop=True))
-
+    def test_join_monthly_data(self): # test the  jointed and the saved data
+        jointed = (Report1.get_joined_monthly_data()).reset_index(drop=True)
+        saved = (pd.read_csv(os.path.join(op_path, new_name_file + '.csv'))).reset_index(drop=True)
+        assert_frame_equal(jointed.reset_index(drop=True), saved.reset_index(drop=True))
+ 
     def test_clean_empty_val(self):
-        cleaned = Report1.clean_empty_val(df)
+        cleaned = Report1.clean_empty_val()
         self.assertEqual(cleaned.isnull().values.any(), False)
+
+
 
 # ----------------------------------------------
 if __name__ == '__main__':
