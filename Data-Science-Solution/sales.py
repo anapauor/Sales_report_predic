bff06431d1b73313bafa798bef3e6ba9723faa62
anapauor <anapaula.orri@gmail.com>
2021-01-20 16:16:13 +0100

    adding set-month method

--- Data-Science-Solution/sales.py
+++ Data-Science-Solution/sales.py
@@ -4,32 +4,50 @@ import os
 
 # Sale resport class for report generation from files 
 
-class SalesReport():
-    """Genererates reports of mothly sales based on cvs data files"""
 
+"""  
+## Programm for creating reports from arranged sales data
+
+FileData cls -> loaded csv files containing monthly sales
+    get_joined_data mtd: return a single file with charged period sales 
+
+SalesReport cls -> Period Reports
+
+
+"""
+
+class FileData():
+    """ 
+    File prep
+    """ 
     def __init__(self, files, path):
         self.files = files # name of the input files
         self.path = path # name of the input path
-        self.all_months_data = self.get_joined_monthly_data()
+        self.all_months_data = pd.DataFrame()
         
     def get_joined_monthly_data(self): 
-        "Joints the monthly sales data in a single file named in 'new_file_name' located in the 'op_path'"
-        self.all_months_data = pd.DataFrame()
+        "Joints the monthly sales data in a single file named in 'new_file_name' located in the 'op_path'"  
         for file in self.files:
             current_data = pd.read_csv(self.path+"/"+file)
             self.all_months_data = pd.concat([self.all_months_data, current_data])
         return self.all_months_data
 
-    def save_joined_monthly_data(self, new_file_name, op_path):
-        self.all_months_data.to_csv((os.path.join(op_path, new_file_name+ '.csv')), index=False)
+    # def save_joined_monthly_data(self, new_file_name, op_path):
+    #     self.all_months_data.to_csv((os.path.join(op_path, new_file_name+ '.csv')), index=False)
+
+class AllMonthData():
+    def __init__(self, all_months_data):
+        self.all_months_data = pd.DataFrame(all_months_data)
+
 
     def clean_empty_val(self): 
         'Pre-processing data'
+        
         self.all_months_data = self.all_months_data.dropna(how='all') # Find and remove NAN
         return self.all_months_data
 
-    # def set_month_column(self):
-    #     self.all_months_data['Month'] = pd.to_datetime(self.all_months_data['Order Date']).dt.month # Add a month column
+    def set_month_column(self):
+        self.all_months_data['Month'] = pd.to_datetime(self.all_months_data['Order Date']).dt.month # Add a month column
 
 
     # def get_address(self):
---
+++ Data-Science-Solution/test_sales.py
@@ -0,0 +1,54 @@
+# usefull modules
+from sales import FileData
+from sales import AllMonthData
+
+import pandas as pd
+from pandas._testing import assert_frame_equal
+import unittest
+import os
+
+# Set Up samples
+
+path_test = "/home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/SalesAnalysis/Sales_Data"
+files_test = [file for file in os.listdir(path_test) if not file.startswith('.')] # Ignore hidden files
+
+file_report = FileData(files=files_test, path=path_test)
+
+new_name_file = 'all_data_sales'
+op_path = '/home/ana/Documentos/Proyectos/Sales/Data-Science-Solution/Output'
+
+saved = (pd.read_csv(os.path.join(op_path, new_name_file + '.csv'))).reset_index(drop=True)
+
+df = AllMonthData(saved)
+
+# -------------------------------> TEST <---------------------------------------------------------------
+
+class TestFileData(unittest.TestCase):
+
+    def test_FileData_files(self):
+        self.assertEqual(file_report.files, files_test)
+    
+    def test_FileData_path(self):
+        self.assertEqual(file_report.path, path_test)
+    
+    def test_save_joined_monthly_data(self):
+    #     Report1.save_joined_monthly_data(new_name_file, op_path)
+        self.assertTrue(os.path.exists(op_path+'/'+new_name_file+'.csv'))
+
+    def test_join_monthly_data(self): # test the  jointed and the saved data
+        assert_frame_equal((file_report.get_joined_monthly_data()).reset_index(drop=True), saved.reset_index(drop=True))
+
+class TestAllMonthData(unittest.TestCase):
+
+    def test_clean_empty_val(self):
+        self.assertFalse((df.clean_empty_val()).isnull().values.any())
+    
+    def test_set_month_column(self):
+        self.assertTrue('Month' in (df.set_month_column()))
+
+
+
+# ----------------------------------------------
+if __name__ == '__main__':
+    unittest.main()
+
